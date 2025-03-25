a4c2fada33e7eddec460235956657b3610105c48
joac001 <jordonez@fi.uba.ar>
2025-03-25 09:03:44 -0300

    Finish him? Still waiting for business name and logo - Got phone number

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -14,7 +14,7 @@ export default function ProductCard({ index, product }) {
         event.preventDefault();
         const message = `Hola, me interesa el ${marca} ${nombre} de ${puffs}puffs sabor ${sabor}. ¿Podrías darme más información?`;
         const encodedMsg = encodeURIComponent(message);
-        window.open(`https://api.whatsapp.com/send?phone=5491130921112&text=${encodedMsg}`, '_blank');
+        window.open(`https://api.whatsapp.com/send?phone=5491138322951&text=${encodedMsg}`, '_blank');
     }
 
     useEffect(() => {
