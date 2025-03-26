a70e413e4dc9589729b56f9976b065972087b728
joac001 <jordonez@fi.uba.ar>
2025-03-26 09:04:31 -0300

    SEO work

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,14 +12,15 @@ export default function RootLayout({ children }) {
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <meta name="keywords" content="vapes, catálogo de vapes, comprar vapes, dispositivos de vapeo, sabores de vapeo" />
+        <meta name="keywords" content="Vaperes Argentina, Vaper Precio Argentina, Vape shop Buenos Aires, Vapers baratos argentina, Vape descartable" />
         <meta name="author" content={metadata.title} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
+        {/* TODO: Poner el dominio comprado por ellos */}
         <meta property="og:url" content="https://vapes-front.vercel.app/" />
-        {/* TODO: poner imagen del logo */}
+        {/* TODO: Poner imagen del logo */}
         <meta property="og:image" content="https://vapes-front.vercel.app/imagen_del_logo" />
       </Head>
       <body>
